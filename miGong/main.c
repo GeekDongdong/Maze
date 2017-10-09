f9aa4c8a55acf48bdfc579cffca6163af59a7a7f
GeekDongdong <zhaodong@xiyou3g.com>
2017-10-09 13:34:10 +0800

    Initial Commit

---
+++ miGong/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  miGong
+//
+//  Created by JACK on 2017/10/9.
+//  Copyright © 2017年 JACK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
